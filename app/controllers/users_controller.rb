fd5abadeb259abf47dd36f1a2016c66682ef5ec9
maeda-seina <ayrton19601027@gmail.com>
2021-07-29 07:33:05 +0900

    find_byでのエラー画面を本番環境で確認

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,15 @@
 class UsersController < ApplicationController
 
-  # usersコントローラのmaedaアクションでaaa.html.erbを表示したい。
-  def maeda
-    render 'aaa'
+  def index
+    @users = User.all
   end
 
-  # usersコントローラのasadaアクションでbbb.html.erbを表示したい。
-  def asada
-    # nameにMikeと入っているユーザーをDBからとってきて、@userに入れる。
-    @user = User.find_by(name:"Mike")
-    render 'bbb'
+  def show
+    @user = User.find_by(id: params[:id])
+    if @user.nil?
+      render action: "index"
+      # redirect_to action: :index
+    end
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,3 @@
 Rails.application.routes.draw do
-  # localhost:3000/seinaでusersコントローラのmaedaアクションを呼びたい
-  get 'seina', to: 'users#maeda'
-  
-  # localhost:3000/sachikoでusersコントローラのasadaアクションを呼びたい
-  get 'sachiko', to:'users#asada'
+  resources :users
 end
